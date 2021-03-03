520cde0fcafbf2ef454868337c9f989558a772f3
PyGuy <75491816+TechnoShip123@users.noreply.github.com>
2021-03-03 16:08:15 -0600

    Minor changes - Added a few more terminal messages, as well as a suggestion command (in progress).

--- README.md
+++ README.md
@@ -13,4 +13,5 @@ If you would like to see more projects from me, make sure to check out my [GitHu
 You will need to provide a `key.txt` file containing the bot key that you generate from your application in the Discord Developer page. This bot will also contain code for slash-commands so you will need to make sure your bot app has access to them.
 
 
-If you plan on using part of or even all of this code for your own bots, you are welcome to do so and modify it as well. However I ask that you credit me and keep the `LICENSE` file intact to comply with the [GNU GPL v3 license](https://github.com/TechnoShip123/Thonk-Bot/blob/master/LICENSE). It may seem annoying but it helps make sure that the work I put into it does not go to waste. Thank you for understanding!
+If you plan on using part of or even all of this code for your own bots, you are welcome to do so and modify it as well. However I ask that you credit me and keep the `LICENSE` file intact to comply with the [GNU GPL v3 license](https://github.com/TechnoShip123/Thonk-Bot/blob/master/LICENSE).
+It may seem annoying, but it helps make sure that the work I put into it does not go to waste. Thank you for understanding!
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 # THONK BOT - Created by TechnoShip123 (Thonk), my GitHub page is at https://github.com/TechnoShip123.
-# This code is under the GNU GPL v3 License.
+# This code is under the GNU GPL v3 License. LICENSE may not be altered or removed.
 
 # Import the needed modules that the bot will use, like discord.py (duh).
 import discord  # For the entire thing
 from discord.ext import commands  # Subset of discord
+from discord.ext import tasks  # Subset of discord
 import sys  # For system commands
 import logging  # For logging TODO: Set up logging
 import traceback  # For Errors
 
 # Here we define the command prefix for the bot, as well as the bot description and intents.
-intents = discord.Intents.default()
-intents.members = True
-bot = commands.Bot(command_prefix = '?!', description = 'A simple bot by TechnoShip123 (Thonk)', intents = intents, case_sensitive=False)
+intents = discord.Intents.default()  # Simplify intents to intents.
+intents.members = True  # Set intents for members to true. This will require verification if the bot is in more than 100 servers.
+
+# Set the bot prefix, description, intents on, and make it case insensitive.
+bot = commands.Bot(command_prefix='?!', description='A simple bot by TechnoShip123 (Thonk)', intents=intents,
+                   case_insensitive=True)
 
 # We also want to get our bot token/key from our `token.txt` file. Putting it in directly in the source code would not be safe.
 with open('token.txt', 'r') as file:
-    TOKEN = file.read().replace('\n', '')  # Assign the token to a variable called TOKEN.
+    TOKEN = file.read().replace('\n', '')  # Assign the token to a str variable called TOKEN.
+
 
 # Here we make a class to add colorful words to the terminal using ANSI Escape Sequences.
 class bcolors:
@@ -36,17 +41,30 @@ class bcolors:
 @bot.event
 async def on_ready():
     # Say that we logged in successfully, and give the username + userid that the bot has logged in as.
-    print(bcolors.OKCYAN + "[INFO]: " + bcolors.ENDC + bcolors.OKGREEN + bcolors.BOLD + "Successful Login! " + bcolors.ENDC)
-    print(bcolors.OKCYAN + "[INFO]: " + bcolors.ENDC + "Logged in as: " + bcolors.OKCYAN + bcolors.HEADER + bcolors.ITALIC + "{bot_username}".format(bot_username = bot.user.name) + bcolors.ENDC)
-    print(bcolors.OKCYAN + "[INFO]: " + bcolors.ENDC + "Bot ID: " + bcolors.OKCYAN + bcolors.HEADER + bcolors.ITALIC + "{bot_user_id}".format(bot_user_id = bot.user.id) + bcolors.ENDC)
-    print('----------------------------------------------------------------------')  # Add a little seperator using hyphens (-).
+    print(
+        bcolors.OKCYAN + "[INFO]: " + bcolors.ENDC + bcolors.OKGREEN + bcolors.BOLD + "Successful Login! " + bcolors.ENDC)
+    print(
+        bcolors.OKCYAN + "[INFO]: " + bcolors.ENDC + "Logged in as: " + bcolors.OKCYAN + bcolors.HEADER + bcolors.ITALIC + "{bot_username}".format(
+            bot_username=bot.user.name) + bcolors.ENDC)
+    print(
+        bcolors.OKCYAN + "[INFO]: " + bcolors.ENDC + "Bot ID: " + bcolors.OKCYAN + bcolors.HEADER + bcolors.ITALIC + "{bot_user_id}".format(
+            bot_user_id=bot.user.id) + bcolors.ENDC)
+
+    # Here we set the bot's status, there is Playing, Watching, Listening to, Streaming, and Competing in.
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.watching, name="your every move | ?!"))
+    # await bot.change_presence(activity=discord.Streaming(name="and watching for ?!", url='https://www.twitch.tv/morshu_beatbox'))
+    print(bcolors.OKCYAN + "[INFO]: " + bcolors.ENDC + bcolors.OKGREEN + "Bot status added! " + bcolors.ENDC)
+    print(
+        '----------------------------------------------------------------------')  # Add a little seperator using hyphens (-).
 
 
 # EVENT - When a new member joins the server say their name and that they joined the server.
 @bot.event
-async def on_member_join(member)
+async def on_member_join(member):
     # TODO: Make it have a green color and say [SERVER]:
-    print(f'{member} has joined the server!')
+    print(
+        bcolors.OKGREEN + "[SERVER]: " + bcolors.ENDC + bcolors.HEADER + f'{member}' + bcolors.ENDC + ' has joined the server!')
 
 
 # COMMAND - Adds two numbers given by the user and seperated by a space.
@@ -58,10 +76,19 @@ async def add(ctx, left: int, right: int):
     # This one could be: `await ctx.send("Invalid input. Try: `?!add 5 10` to add 5 + 10.")` (don't forget return)
 
 
+# COMMAND - Saves the command idea given from someone to a file.
+@bot.command(name="commandidea", aliases=["csuggest", "command_suggestion", "cidea"],
+             help="Suggests a command for me to add to the bot.")
+async def command_suggestion(ctx):
+    channel = bot.get_channel(816422364681470013)  # Define my suggestion channel.
+    await channel.send(ctx.message.content)  # Send the suggestion to my suggestion channel
+    await ctx.message.add_reaction('<a:checkmark:806962593457504296>')
+    await ctx.send("Your suggestion: ```" + ctx.message.content + "``` has been sent to Thonks idea pastebin!")
+
+
 # ERROR HANDLING ------------------------------------------------------------------------------------------------------------------
 @bot.event
 async def on_command_error(ctx, error):
-
     # If the command has local error handler, then just return.
     if hasattr(ctx.command, 'on_error'):
         return
@@ -124,14 +151,15 @@ async def on_command_error(ctx, error):
         await ctx.send("You do not have permission to use this command.")
         return
 
-
     # Ignore all other exception types, but print them to stderr
-    print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+    print(bcolors.FAIL + "[ERROR]: " + bcolors.ENDC + bcolors.WARNING + 'Ignoring exception in command {}:'.format(
+        ctx.command) + bcolors.ENDC, file=sys.stderr)
 
     traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 
 # RUN CODE ------------------------------------------------------------------------------------------------------------------------
-# Here is the final step, where start to run our bot.
-# TODO: Print: preparing to run bot with [info]: and all the colors? Just for a little more 'decoration'.
+
+# Here is the final step, where we start to run our bot.
+print(bcolors.OKCYAN + "[INFO]: " + bcolors.ENDC + bcolors.OKBLUE + bcolors.ITALIC + "Starting Bot... " + bcolors.ENDC)
 bot.run(TOKEN)
