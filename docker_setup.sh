51a4485cb9032a4e528d9bade7bc5f5c750ad65e
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2021-03-19 13:04:17 -0500

    Make Docker Setup shell script
    because linux is cool

---
+++ docker_setup.sh
@@ -0,0 +1,20 @@
+docker stop thonk-bot
+sleep 2
+
+docker container rm thonk-bot
+docker container ls
+sleep 1
+
+docker image rm thonk-bot
+echo 'Removed old thonk-bot images!'
+sleep 1
+
+docker build -t thonk-bot .
+echo 'Docker image succesfully built!'
+sleep 1
+
+docker images
+sleep 2
+echo 'Running newly generated Docker image in container!'
+
+docker run --name thonk-bot -it thonk-bot launcher.py
