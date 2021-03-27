51db7fdd4bdb5990e01e9497be6611abeee0c925
PyGuy <75491816+TechnoShip123@users.noreply.github.com>
2021-03-27 13:57:06 -0500

    Basic slash commands created, not implemented into main bot yet though, it can be found in test.py, I also improved the ?!info command, gives the docs page and the repository page, and can be used to tell other people to look by specifying a user. New dependency package was added to the Dockerfile as well.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3
 WORKDIR /usr/src/app
 COPY . .
-RUN pip install discord[voice] PyNaCl requests datetime asyncio apscheduler psutil
+RUN pip install discord[voice] PyNaCl requests datetime asyncio apscheduler psutil discord-py-slash-command
 CMD ["launcher.py"]
 ENTRYPOINT ["python3"]
--- test.py
+++ test.py
@@ -1,51 +1,45 @@
-from discord.ext import commands
-import discord.ext.tasks
-from datetime import *
 import discord
-import asyncio
-
-bot = commands.Bot(command_prefix='?!', description='A simple bot by TechnoShip123 (Thonk).', case_insensitive=True)
+from discord.ext import commands
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option
 
 with open("./lib/bot/token.txt", "r", encoding="utf-8") as tokenfile:
-    TOKEN = tokenfile.read()
+    TOKEN = tokenfile.readline()
+
+bot = commands.Bot(command_prefix="?!", intents=discord.Intents.all())
+slash = SlashCommand(bot, sync_commands=True)
 
 
-@bot.command(name="remind")
-async def remind(ctx, time, *, reminder):
-    print(time)
-    print(reminder)
-    user = "<@!" + str(ctx.author.id) + ">"
-    embed = discord.Embed(color=0x55a7f7, timestamp=datetime.utcnow())
-    embed.set_footer(text="Requested by: " + "<@!" + str(ctx.author.id) + ">", icon_url=f"{bot.user.avatar_url}")
-    seconds = 0
-    if reminder is None:
-        embed.add_field(name='Warning', value='Please specify what do you want me to remind you about.')  # Error message
-    if time.lower().endswith("d"):
-        seconds += int(time[:-1]) * 60 * 60 * 24
-        counter = f"{seconds // 60 // 60 // 24} day(s)"
-    if time.lower().endswith("h"):
-        seconds += int(time[:-1]) * 60 * 60
-        counter = f"{seconds // 60 // 60} hour(s)"
-    elif time.lower().endswith("m"):
-        seconds += int(time[:-1]) * 60
-        counter = f"{seconds // 60} minute(s)"
-    elif time.lower().endswith("s"):
-        seconds += int(time[:-1])
-        counter = f"{seconds} second(s)"
-    if seconds == 0:
-        embed.add_field(name='Invalid Duration!',
-                        value='Please specify a proper duration, `?!remind <time> <name>`. For example, `?!remind 5s Coding` for a reminder in 5 seconds.')
-    elif seconds < 5:
-        embed.add_field(name='Duration Too Small!',
-                        value='You have specified a too short duration!\nThe minimum duration is 5 seconds.')
-    elif seconds > 604800:
-        embed.add_field(name='Duration Too Large!', value='You have specified too long of a duration!\nThe maximum duration is 7 days.')
+@bot.event
+async def on_ready():
+    print("Ready!")
+
+
+guild_ids = [764981968579461130]  # Put your server ID in this array.
+
+
+@slash.slash(name="test", guild_ids=guild_ids,
+             description="Is thonk cool?",
+             options=[
+                 create_option(
+                     name="option",
+                     description="Yes or No.",
+                     option_type=3,
+                     required=False
+                 )
+             ])
+async def test(ctx, *, option: [str] = None):
+    if option is None:
+        await ctx.send("Test is working.")
     else:
-        await ctx.reply(f"Alright, I will remind you about {reminder} in {counter}.")
-        await asyncio.sleep(seconds)
-        await ctx.send(f"Hey {user}, you asked me to remind you about {reminder} {counter} ago.")
-        return
-    await ctx.send(embed=embed)
+        if option.lower() == "yes":
+            await ctx.send(f"You said {option}, thanks! :D")
+        elif option.lower() == "no":
+            await ctx.send(f"<:stevegun:815628826767523840> You said {option} :/")
+        elif option.lower() == "e":
+            await ctx.send(f"<:nice:817119421445046293> **E.**")
+        else:
+            await ctx.send(f"I have no idea tf you said by `{option}`, but I'm assuming yes! :D")
 
 
 bot.run(TOKEN)
